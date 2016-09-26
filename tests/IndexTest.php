b882c0c34e8bc7708600c0d3b68df239f22bba47
lucy <lucy@esesglobal.com>
2016-09-26 16:51:14 +0900

    add phpunit Test

---
+++ tests/IndexTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Test;
+
+class IndexTest extends \UnitTestCase
+{
+    public function testHello()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+}
